e511c1674698e78846467da982fd623ad4d26150
PolinaVasilevich <polina.vasilevichh@gmail.com>
2021-12-27 14:18:16 +0300

    setup frontend and backend; set router

---
+++ shareme_frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import AppRouter from "./components/AppRouter";
+
+const App: FC = () => {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
---
+++ shareme_frontend/src/routes/index.ts
@@ -0,0 +1,27 @@
+import App from "../App";
+import Home from "../container/Home";
+import Login from "../container/Login";
+
+export enum RoutePath {
+  HOME = "/",
+  LOGIN = "/login",
+}
+
+export interface IRoute {
+  name: string;
+  path: RoutePath;
+  component: React.ComponentType;
+}
+
+export const routes: IRoute[] = [
+  {
+    name: "login",
+    path: RoutePath.LOGIN,
+    component: Login,
+  },
+  {
+    name: "home",
+    path: RoutePath.HOME,
+    component: Home,
+  },
+];
